b16a5eb355605c63fc127e43126e3445d3f1e7a9
def.gsus- <stefan.berke@modular-audio-graphics.com>
2016-07-11 13:01:35 +0200

    start Qt-port

---
+++ mainwindow.cpp
@@ -0,0 +1,24 @@
+/** @file mainwindow.cpp
+
+    @brief
+
+    <p>(c) 2016, stefan.berke@modular-audio-graphics.com</p>
+    <p>All rights reserved</p>
+
+    <p>created 7/11/2016</p>
+*/
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ mainwindow.h
@@ -0,0 +1,32 @@
+/** @file mainwindow.h
+
+    @brief
+
+    <p>(c) 2016, stefan.berke@modular-audio-graphics.com</p>
+    <p>All rights reserved</p>
+
+    <p>created 7/11/2016</p>
+*/
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
